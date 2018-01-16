810f3c22014633a25b4560276b539bb8213a9124
Taniadz <tdziubenko@ukr.net>
2018-01-16 12:45:42 +0200

    changes for deploy2

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -s /home/tania/PycharmProjects/fish/etc/nginx.conf   /etc/nginx/sites-enabled/fish.conf
+sudo ln -s /home/ubuntu/app/etc/nginx.conf   /etc/nginx/sites-enabled/fish.conf
 sudo ln -s /etc/nginx/sites-available/fish.conf /etc/nginx/sites-enabled
 sudo systemctl reload nginx
 
-sudo ln -s /home/tania/Pycd nginxcharmProjects/fish/etc/application.service /etc/systemd/system/fish.service
+sudo ln -s /home/ubuntu/app/etc/application.service /etc/systemd/system/fish.service
 sudo systemctl start fish
