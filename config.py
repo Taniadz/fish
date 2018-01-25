057597580a8fc580d23a4064f579164bba04bf9d
Taniadz <tdziubenko@ukr.net>
2018-01-25 23:35:43 +0200

    configuration for python social auth

--- config.py
+++ config.py
@@ -26,23 +26,22 @@ SOCIAL_AUTH_FACEBOOK_SECRET= '988af003ce0c2f1b35140a80cd146cdb'
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
   'fields': 'id, name, email, age_range'
 }
-
-# app.config["SOCIAL_AUTH_PIPELINE"] = (
-#     'application.views.save_profile',
-#     'social_core.pipeline.social_auth.social_details',
-#     'social_core.pipeline.social_auth.social_uid',
-#     'social_core.pipeline.social_auth.auth_allowed',
-#     'social_core.pipeline.social_auth.social_user',
-#     'social_core.pipeline.user.get_username',
-#     'common.pipeline.require_email',
-#     'social_core.pipeline.mail.mail_validation',
-#     'social_core.pipeline.user.create_user',
-#     'social_core.pipeline.social_auth.associate_user',
-#     'social_core.pipeline.debug.debug',
-#     'social_core.pipeline.social_auth.load_extra_data',
-#     'social_core.pipeline.user.user_details',
-#     'social_core.pipeline.debug.debug'
-# )
+SOCIAL_AUTH_PIPELINE = (
+    'application.views.save_profile',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'common.pipeline.require_email',
+    #'social_core.pipeline.mail.mail_validation',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    #'social_core.pipeline.debug.debug',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    #'social_core.pipeline.debug.debug'
+)
 #
 
 
